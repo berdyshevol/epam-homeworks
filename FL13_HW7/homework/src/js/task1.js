2e63ca864c8ecfc8156ba237aa545e51c7ab5419
berdyshevol <berdyshevo@gmail.com>
2020-04-23 22:24:28 +0300

    HW7-1

---
+++ FL13_HW7/homework/src/js/task1.js
@@ -0,0 +1,68 @@
+const alertMessage = {
+    'is an empty line or Esc': 'Canceled.',
+    'length less than 4 symbols': 'I don\'t know any users having name length less than 4 symbols',
+    'another string': 'I don’t know you',
+    'Wrong password': 'Wrong password'
+}
+const users = {
+    'User': 'UserPass',
+    'Admin': 'RootPass'
+};
+
+const userGreetLessThan20 = {
+    'User': 'Good day, dear User!',
+    'Admin': 'Good day, dear Admin!'
+};
+
+const userGreetMoreThan20 = {
+    'User': 'Good evening, dear User!',
+    'Admin': 'Good evening, dear Admin!'
+};
+
+const login = prompt('Input login');
+if (checkLogin(login)) {
+    const password = prompt('Input password');
+    if (checkPassword(login, password)) {
+        greetsUserAppropriately(login);
+    }
+}
+
+function checkLogin(login) {
+    if (!login) {
+    // if (login === '' || login === null) {
+        showMessage('is an empty line or Esc');
+        return false;
+    } else if (login.length < 4) {
+        showMessage('length less than 4 symbols');
+        return false;
+    } else if (login in users) {
+        return true;
+    } else {
+        showMessage('another string');
+        return false;
+    }
+}
+
+function checkPassword(login, password) {
+    if (password === null || password === '') {
+        showMessage('is an empty line or Esc');
+        return false;
+    } else if (users[login] === password) {
+        return true;
+    } else {
+        showMessage('Wrong password');
+    }
+}
+
+function greetsUserAppropriately(login) {
+    const hour = new Date().getHours();
+    if (hour < 20) {
+        alert(userGreetLessThan20[login]);
+    } else {
+        alert(userGreetMoreThan20[login]);
+    }
+}
+
+function showMessage(key) {
+    alert(alertMessage[key]);
+}
---
+++ FL13_HW7/homework/src/js/task2.js
@@ -0,0 +1,2 @@
+// Your code goes here
+const confirm = confirm('Do you want to play a game?');
